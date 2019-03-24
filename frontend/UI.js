4f8015be0a28b673ad9c2307dae9652f745724f1
Luis Rodriguez <developer0606@gmail.com>
2019-03-24 10:31:26 -0400

    [ADD] UI class To DOM

--- frontend/UI.js
+++ frontend/UI.js
@@ -1,7 +1,54 @@
+import BookService from './services/BookService';
+const bookService = new BookService();
+
+import { format } from 'timeago.js';
+
 class UI {
-    renderBooks() { }
-    addANewBooks() { }
-    ClearBookForm() { }
+
+    async renderBooks() {
+        const container = document.getElementById('books-cards');
+        const books = await bookService.getBooks().then(res => {
+            container.innerHTML = '';
+            res.data.forEach(book => {
+                const div = document.createElement('div');
+                div.className = '';
+                div.innerHTML = `
+                    <div class="card m-2">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="http://localhost:3000/${book.imagePath}" alt="${book.imagePath}" class="img-fluid" />
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-block px-2"> 
+                                    <h4 class="card-title">${book.title}</h4>
+                                    <p class="card-text">${book.author}</p>
+                                    <a href="#" class="btn btn-danger delete" _id="${book._id}">X</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            ${format(book.created_at)}
+                        </div>
+                    </div>
+                `;
+                container.appendChild(div);
+            });
+        });
+    }
+
+    async addANewBook(book) {
+
+        console.log('add book:', book);
+        await bookService.postBooks(book).then(res => console.log(res));
+        this.renderBooks();
+        this.ClearBookForm();
+
+    }
+
+    ClearBookForm() {
+        document.getElementById('book-form').reset();
+    }
+
     renderMessage() { }
     deleteBook() { }
 }
--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,12 @@
 // require('./styles/style.css');
 import './styles/app.css';
-import BookService from './services/BookService';
+import UI from './UI';
 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('LOADED DOM');
+    const ui = new UI();
+    ui.renderBooks();
+});
 
 document.getElementById('book-form').addEventListener('submit', e => {
     e.preventDefault();
@@ -16,6 +21,8 @@ document.getElementById('book-form').addEventListener('submit', e => {
     data.append('isbn', isbn);
     data.append('image', image[0]);
 
-    const bookService = new BookService();
-    const res = bookService.postBooks(data).then(res => console.log(res));
+    const ui = new UI();
+    ui.addANewBook(data);
+
+
 });
